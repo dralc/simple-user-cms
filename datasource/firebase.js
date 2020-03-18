fd185911198541d5ef1a27753a4ac4e12a17fe0e
dralc <dr.aloysius@gmail.com>
2020-03-19 09:44:33 +1100

    added abstract datasource added concrete firebase datasource

---
+++ datasource/firebase.js
@@ -0,0 +1,41 @@
+require('../types');
+const fetch = require('node-fetch');
+const endpoint = process.env.FB_ENDPOINT;
+
+/**
+ * @param {User} user
+ * @returns {Promise<AddedUser>
+ */
+exports.add = async user => {
+	const res = await fetch(`${endpoint}.json`, {
+		method: 'post',
+		body: JSON.stringify(user),
+		headers: { 'Content-Type': 'application/json' },
+	})
+
+	return res.json();
+}
+
+/**
+ * @param {String} id
+ * @returns {String}
+ */
+exports.delete = async id => {
+	await fetch(`${endpoint}/${id}.json`, {
+		method: 'delete',
+	});
+
+	return 'ok';
+}
+
+/**
+* @param {String} name 
+* @returns {Promise<User>}
+ */
+exports.get = async name => {
+	const res = await fetch(`${endpoint}.json?orderBy="name"&startAt="${name}"&endAt="${name}\\uf8ff"`, {
+		method: 'get',
+	});
+
+	return res.json();
+}
---
+++ types.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef {Object} User
+ * @property {String} name
+ * @property {String} email
+ * @property {String} address
+ * @property {Boolean} role
+ */
+
+ /**
+  * @typedef {Object} AddedUser
+  * @property {String} name
+  */
