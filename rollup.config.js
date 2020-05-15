8612b6010f3cd47cbfd7db3bd6a19c46def25984
dralc <dr.aloysius@gmail.com>
2020-05-15 15:24:52 +1000

    Use global nodemon for dev:api Use dotenv to set local env for tests Tidy npm scripts

--- rollup.config.js
+++ rollup.config.js
@@ -61,7 +61,7 @@ function serve() {
 			if (!started) {
 				started = true;
 
-				require('child_process').spawn('npm', ['run', 'start:fe', '--', '--dev'], {
+				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
 					stdio: ['ignore', 'inherit', 'inherit'],
 					shell: true
 				});
